2343f2b1b744beb8a2bd0751fdfee3b7bfca379b
c29r3 <52304443+c29r3@users.noreply.github.com>
2022-04-16 04:12:24 +0300

    Create redelegate.sh

---
+++ redelegate.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+WALLET_NAME="osmosis"
+BIN_FILE="$HOME/go/bin/osmosisd"
+TOKEN="uosmo"
+RPC="http://localhost:26657"
+TX_FEE="5000"
+CHAIN_ID=$(curl -s $RPC/status | jq -r .result.node_info.network)
+read -sp "Password: " WALLET_PWD
+
+SELF_ADDR=$(echo -e "$WALLET_PWD" | $BIN_FILE keys list --output json | jq -r ".[] | select(.name == \"$WALLET_NAME\").address")
+OPERATOR=$(echo -e "$WALLET_PWD" | $BIN_FILE keys show $WALLET_NAME --bech val --output json | jq -r .address)
+
+
+while true; do
+    # withdraw reward
+    echo -e "$WALLET_PWD" | $BIN_FILE tx distribution withdraw-rewards $OPERATOR --fees $TX_FEE$TOKEN --commission --chain-id $CHAIN_ID --from $WALLET_NAME --node ${RPC} -y
+
+    sleep 10
+
+    # check current balance
+    BALANCE=$($BIN_FILE q bank balances $SELF_ADDR -o json --node ${RPC} | jq -r ".balances[] | select(.denom == \"$TOKEN\").amount")
+    echo CURRENT BALANCE IS: $BALANCE
+
+    RESTAKE_AMOUNT=$(echo "$BALANCE - 1000000" | bc)
+
+    if (( $(bc <<< "$RESTAKE_AMOUNT >=  2000000") ));then
+        echo "Let's delegate $RESTAKE_AMOUNT of REWARD tokens to $SELF_ADDR"
+        # delegate balance
+        echo -e "$WALLET_PWD" | $BIN_FILE tx staking delegate $OPERATOR "$RESTAKE_AMOUNT"$TOKEN --fees $TX_FEE$TOKEN --chain-id $CHAIN_ID --from $WALLET_NAME --node ${RPC} -y
+
+    else
+        echo "Reward is $RESTAKE_AMOUNT"
+    fi
+    echo "DONE"
+    sleep 10800
+done
